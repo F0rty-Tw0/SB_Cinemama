f37a4daf04649f02add66e9de705211f54dfae5f
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-12 14:34:23 +0200

    first commit

---
+++ src/main/java/mandatory/cinemama/CinemamaApplication.java
@@ -0,0 +1,13 @@
+package mandatory.cinemama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mandatory/cinemama/CinemamaApplicationTests.java
@@ -0,0 +1,13 @@
+package mandatory.cinemama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
