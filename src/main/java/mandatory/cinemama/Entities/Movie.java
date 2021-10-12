d168ff22c4ad1c1ba289ae79fdd776b2f29f28d4
pawe0773 <pawe0773@stud.kea.dk>
2021-10-12 16:22:21 +0200

    changed import style

--- src/main/java/mandatory/cinemama/Entities/Movie.java
+++ src/main/java/mandatory/cinemama/Entities/Movie.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import java.time.LocalTime;
 
