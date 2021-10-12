b109f05131fd8d70c1f7fbe7d6e8d4ccde3a2f8e
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-12 20:27:32 +0200

    created app propreties for tests

--- src/test/java/mandatory/cinemama/CinemamaApplicationTests.java
+++ src/test/java/mandatory/cinemama/CinemamaApplicationTests.java
@@ -2,12 +2,14 @@ package mandatory.cinemama;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application.properties")
 class CinemamaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
