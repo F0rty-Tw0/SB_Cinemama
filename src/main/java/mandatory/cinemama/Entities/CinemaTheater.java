31457d3dd60fe0aacdda23518b378ac2db5bf644
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-12 16:30:08 +0200

    merged all branches

--- src/main/java/mandatory/cinemama/Entities/CinemaTheater.java
+++ src/main/java/mandatory/cinemama/Entities/CinemaTheater.java
@@ -1,8 +1,6 @@
 package mandatory.cinemama.Entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
