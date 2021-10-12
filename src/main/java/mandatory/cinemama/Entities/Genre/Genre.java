87a7c0a49526ac3f48481563fe7250c338469681
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-12 16:15:52 +0200

    created enum for genres

---
+++ src/main/java/mandatory/cinemama/Entities/Genre/Genre.java
@@ -0,0 +1,31 @@
+package mandatory.cinemama.Entities.Genre;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "genres", schema = "cinemama")
+public class Genre {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private EGenre name;
+
+  public Genre(EGenre name) {
+    this.name = name;
+  }
+}
