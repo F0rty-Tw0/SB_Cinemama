9a8ca385c63724587493ae68cefb6b6f106c576b
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2021-10-12 15:13:58 +0200

    Made entity for CinemaHall and CinemaTheater

---
+++ src/main/java/mandatory/cinemama/Entities/CinemaHall.java
@@ -0,0 +1,22 @@
+package mandatory.cinemama.Entities;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+//@Table(name = "CinemaHalls", schema = "?")
+public class CinemaHall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private List<movieSchedules> movieSchedules;
+
+}
---
+++ src/main/java/mandatory/cinemama/Entities/CinemaTheater.java
@@ -0,0 +1,23 @@
+package mandatory.cinemama.Entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+//@Table(name = "CinemaHalls", schema = "?")
+public class CinemaTheater {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String address;
+    private List<CinemaHall> cinemaHalls;
+}
