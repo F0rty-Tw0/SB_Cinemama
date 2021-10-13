714ce89e439142e6cf8d627deb113f506f0ce351
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-13 10:25:23 +0200

    added initial tasks for db config

--- src/main/java/mandatory/cinemama/Configurations/DatabaseConfiguration.java
+++ src/main/java/mandatory/cinemama/Configurations/DatabaseConfiguration.java
@@ -1,5 +1,20 @@
 package mandatory.cinemama.Configurations;
 
-public class DatabaseConfiguration {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component 
+@Profile("main")
+public class DatabaseConfiguration implements CommandLineRunner{
+
+  public DatabaseConfiguration() {
+    // TODO: Add repositories
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+   // TODO: Setup data to database
+  }
   
 }
