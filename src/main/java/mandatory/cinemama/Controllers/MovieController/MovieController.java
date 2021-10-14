dc2311b3745c250ab33e6a1dab922cf614dd145d
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-14 16:00:21 +0200

    fixed pawels anotations and added @Transactional to delete

--- src/main/java/mandatory/cinemama/Controllers/MovieController/MovieController.java
+++ src/main/java/mandatory/cinemama/Controllers/MovieController/MovieController.java
@@ -1,51 +1,43 @@
 package mandatory.cinemama.Controllers.MovieController;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
 
 import mandatory.cinemama.Entities.Movie;
 import mandatory.cinemama.Services.MovieService.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/movies")
 public class MovieController implements MovieControllerInterface {
 
-    @Autowired
-    private MovieService movieService;
+  @Autowired
+  private MovieService movieService;
 
+  @Override
+  public Movie findMovieById(Long id) {
+    return movieService.findMovieById(id);
+  }
 
+  @Transactional
+  @Override
+  public void deleteMovieById(Long id) {
+    movieService.deleteMovieById(id);
+  }
 
-   @Override
-   public Movie findMovieById(@RequestParam(required = false) String type, @PathVariable Long id){
-       return movieService.findMovieById(id);
-   }
+  @Override
+  public List<Movie> findAllMovies() {
+    return movieService.findAllMovies();
+  }
 
-   @Override
-    public void deleteMovieById(@PathVariable Long id){
-        movieService.deleteMovieById(id);
-   }
-
-   @Override
-   public List<Movie> findAllMovies(){
-    return findAllMovies();
-   }
-
-   @Override
-   public Movie findMovieByTitle(@RequestParam(required = false) String type, @PathVariable String title){
+  @Override
+  public Movie findMovieByTitle(String title) {
     return movieService.findMovieByTitle(title);
-   }
+  }
 
-   @Override
-       public Movie addMovie(@RequestBody Movie movie){
-       return movieService.addMovie(movie);
-   }
-
-
-
+  @Override
+  public Movie addMovie(Movie movie) {
+    return movieService.addMovie(movie);
+  }
 }
--- src/main/java/mandatory/cinemama/Controllers/MovieController/MovieControllerInterface.java
+++ src/main/java/mandatory/cinemama/Controllers/MovieController/MovieControllerInterface.java
@@ -1,40 +1,34 @@
 package mandatory.cinemama.Controllers.MovieController;
 
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import mandatory.cinemama.Entities.Movie;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
-
-
+@RequestMapping("/api/movies")
 public interface MovieControllerInterface {
+  @ApiOperation("Returns the movie based on the ID")
+  @GetMapping("/{id}")
+  public Movie findMovieById(@PathVariable Long id);
 
-    @ApiOperation("Returns the movie based on the ID")
-    @GetMapping("/{id}")
-    public Movie findMovieById(@RequestParam(required = false) String type, @PathVariable Long id);
+  @ApiOperation("Deletes the movie based on the ID")
+  @DeleteMapping("/{id}")
+  public void deleteMovieById(@PathVariable Long id);
 
-    @ApiOperation("Deletes the movie based on the ID")
-    @DeleteMapping("/{id}")
-    public void deleteMovieById(@PathVariable Long id);
+  @ApiOperation("Returns all the movies")
+  @GetMapping
+  public List<Movie> findAllMovies();
 
-    @ApiOperation("Returns all the movies")
-    @GetMapping
-    public List<Movie> findAllMovies();
+  @ApiOperation("Returns the movies based on the title")
+  @GetMapping("/title/{title}")
+  public Movie findMovieByTitle(@PathVariable String title);
 
-    @ApiOperation("Returns the movies based on the title")
-    @GetMapping("/title/{title}")
-    public Movie findMovieByTitle(@RequestParam(required = false) String type, @PathVariable String title);
-
-    @ApiOperation("Adds the movie to the database")
-    @PostMapping()
-    public Movie addMovie(@RequestBody Movie movie);
-
+  @ApiOperation("Adds the movie to the database")
+  @PostMapping
+  public Movie addMovie(@RequestBody Movie movie);
 }
