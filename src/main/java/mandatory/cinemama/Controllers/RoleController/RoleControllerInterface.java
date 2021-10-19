9c43a29e61bd231c57d5f3e7ac87c8736a7a335f
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-19 21:27:16 +0200

    finished with description of the endpoints

--- src/main/java/mandatory/cinemama/Controllers/RoleController/RoleControllerInterface.java
+++ src/main/java/mandatory/cinemama/Controllers/RoleController/RoleControllerInterface.java
@@ -2,23 +2,43 @@ package mandatory.cinemama.Controllers.RoleController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import java.util.List;
 import mandatory.cinemama.Entities.User.Role;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Api(tags = "Roles")
+@Api(
+  tags = "Roles (OPTIONAL)",
+  description = "- A secured endpoint for <b>Roles</b>, requires a role of <b>ADMIN</b> to operate! - <em>(This endpoint was created for the testing and learning purposes only).</em>"
+)
 @RequestMapping("/api/roles")
 public interface RoleControllerInterface {
-  @ApiOperation("Returns all Roles available in the database")
+  @ApiOperation(
+    value = " - Returns all Roles available in the database",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Execute to retrieve all <b>Roles</b>."
+  )
   @GetMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public List<Role> findAllRoles();
 
-  @ApiOperation("Returns the Role by Id")
+  @ApiOperation(
+    value = " - Returns the Role by Id",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Enter the <b>id</b> of a Role to retrieve a <b>Role</b> Object."
+  )
   @GetMapping("/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
   public Role findRoleById(Long id);
 
-  @ApiOperation("Returns the Role by Name")
+  @ApiOperation(
+    value = " - Returns the Role by Name",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Enter the <b>Name</b> of a Role to retrieve a <b>Role</b> Object."
+  )
   @GetMapping("/name/{name}")
+  @PreAuthorize("hasRole('ADMIN')")
   public Role findRoleByName(String name);
 }
--- src/main/java/mandatory/cinemama/Controllers/RowController/RowControllerInterface.java
+++ src/main/java/mandatory/cinemama/Controllers/RowController/RowControllerInterface.java
@@ -2,27 +2,52 @@ package mandatory.cinemama.Controllers.RowController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import java.util.List;
 import mandatory.cinemama.Entities.Hall.HallRow.Row;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Api(tags = "Rows")
+@Api(
+  tags = "Rows (OPTIONAL)",
+  description = "- A secured endpoint for <b>Rows</b>, requires a role of <b>ADMIN</b> to operate! - <em>(This endpoint was created for the testing and learning purposes only).</em>"
+)
 @RequestMapping("/api/rows")
 public interface RowControllerInterface {
-  @ApiOperation("Returns all Rows available in the database")
+  @ApiOperation(
+    value = " - Returns all Rows available in the database",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Execute to retrieve all <b>Rows</b>."
+  )
   @GetMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public List<Row> findAllRows();
 
-  @ApiOperation("Returns the Row by Id")
+  @ApiOperation(
+    value = " - Returns the Row by Id",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Enter the <b>id</b> of a Row to retrieve a <b>Row</b> Object."
+  )
   @GetMapping("/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
   public Row findRowById(Long id);
 
-  @ApiOperation("Returns the Row by Name")
+  @ApiOperation(
+    value = " - Returns the Row by Name",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Enter the <b>Name</b> of a Row to retrieve a <b>Row</b> Object."
+  )
   @GetMapping("/name/{name}")
+  @PreAuthorize("hasRole('ADMIN')")
   public Row findRowByName(String name);
 
-  @ApiOperation("Returns the Rows by Hall Id")
+  @ApiOperation(
+    value = " - Returns the Rows by Hall Id",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Enter the <b>Hall Id</b> of a Hall to retrieve a list of <b>Rows</b>."
+  )
   @GetMapping("/hall/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
   public List<Row> findRowsByHallId(Long id);
 }
