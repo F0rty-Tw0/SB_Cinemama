5b8fb8caea46ebf218bf7ce547afad9c36346b67
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-19 20:51:11 +0200

    added genre swagger info

--- src/main/java/mandatory/cinemama/Controllers/GenreController/GenreControllerInterface.java
+++ src/main/java/mandatory/cinemama/Controllers/GenreController/GenreControllerInterface.java
@@ -2,22 +2,43 @@ package mandatory.cinemama.Controllers.GenreController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import java.util.List;
 import mandatory.cinemama.Entities.Genre.Genre;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Api(tags = "Genres")
+
+@Api(
+  tags = "Genres (OPTIONAL)",
+  description = "- A secured endpoint for <b>Genres</b>, requires a role of <b>ADMIN</b> to operate! - <em>(This endpoint was created for the testing and learning purposes only).</em>"
+)
 @RequestMapping("/api/genres")
 public interface GenreControllerInterface {
-  @ApiOperation("Returns all Genres available in the database")
+  @ApiOperation(
+    value = " - Returns all Genres available in the database",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Execute to retrieve all <b>Genres</b>."
+  )
   @GetMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public List<Genre> findAllGenres();
 
-  @ApiOperation("Returns the Genre by Id")
+  @ApiOperation(
+    value = " - Returns the Genre by Id",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Enter the <b>id</b> of a Genre to retrieve a <b>Genre</b> Object."
+  )
   @GetMapping("/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
   public Genre findGenreById(Long id);
 
-  @ApiOperation("Returns the Genre by Name")
+  @ApiOperation(
+    value = " - Returns the Genre by Name",
+    authorizations = { @Authorization(value = "jwtToken") },
+    notes = "Enter the <b>Name</b> of a Genre to retrieve a <b>Genre</b> Object."
+  )
   @GetMapping("/name/{name}")
+  @PreAuthorize("hasRole('ADMIN')")
   public Genre findGenreByName(String name);
 }
