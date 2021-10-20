8fa60f3404da0f5f9d26a279e877b8aee7e8b5ad
Artiom Tofan <artiomtofancv@gmail.com>
2021-10-20 17:29:44 +0200

    added reservations

--- src/main/java/mandatory/cinemama/Entities/User/User.java
+++ src/main/java/mandatory/cinemama/Entities/User/User.java
@@ -1,15 +1,21 @@
 package mandatory.cinemama.Entities.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import mandatory.cinemama.Entities.Reservation;
 
 @Getter
 @Setter
@@ -31,6 +37,9 @@ public class User {
 
   @ManyToOne
   private Role role;
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+  private List<Reservation> reservations = new ArrayList<Reservation>();
 
   public User(String email, String password) {
     this.email = email;
