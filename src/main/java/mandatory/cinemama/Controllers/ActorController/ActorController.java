793d07bccdbdd77b43b01bee5b7c8fa285b2c0c3
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2021-10-24 14:57:38 +0200

    testing if jenkins will build pt 4

--- src/main/java/mandatory/cinemama/Controllers/ActorController/ActorController.java
+++ src/main/java/mandatory/cinemama/Controllers/ActorController/ActorController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ActorController implements ActorControllerInterface {
 
+  // pls work jenkins
   @Autowired
   private ActorService actorService;
 
