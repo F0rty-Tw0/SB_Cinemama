b4e94bd0747351ba4f70a0eccc2e4b4da25f1ac8
Artiom Tofan <artiomtofancv@gmail.com>
2021-12-02 22:53:22 +0100

    removed the maximum pool size

--- src/main/java/mandatory/cinemama/Controllers/UserController/UserController.java
+++ src/main/java/mandatory/cinemama/Controllers/UserController/UserController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(
-  origins = { "http://localhost:3000", "https://cinemama.vercel.app" },
-  maxAge = 3600
-)
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class UserController implements UserControllerInterface {
 
