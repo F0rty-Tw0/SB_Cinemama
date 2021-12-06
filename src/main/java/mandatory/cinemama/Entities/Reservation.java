f4428e3636a6f4cb4d35198df66e7cc5c909fc33
Artiom Tofan <artiomtofancv@gmail.com>
2021-12-06 23:09:33 +0100

    added unique constraint

--- src/main/java/mandatory/cinemama/Entities/Reservation.java
+++ src/main/java/mandatory/cinemama/Entities/Reservation.java
@@ -1,16 +1,16 @@
 package mandatory.cinemama.Entities;
 
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -36,6 +36,11 @@ public class Reservation {
   private Schedule schedule;
 
   @ManyToMany
+  @JoinTable(
+    uniqueConstraints = {
+      @UniqueConstraint(columnNames = { "seats_id", "reservation_id" }),
+    }
+  )
   private List<Seat> seats;
 
   @ManyToOne
